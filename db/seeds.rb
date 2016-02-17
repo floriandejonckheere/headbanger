5916f2e134641b3567ec3512816286edad3dab84
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-02-17 13:35:09 +0100

    Genre seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'yaml'
+
+puts 'Seeding database'
+
+genres = YAML.load_file Rails.root.join('db', 'seeds', 'genres.yml')
+genres.keys.each do |genre|
+  Graph::Genre.create(:name => genres[genre]['name'])
+end
+
+puts 'Done seeding database'
