afabd039b4100f11674967dc564e6ed58d75e8e7
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-04-14 21:12:28 +0200

    Sidekiq inlining

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
+if Rails.env.development?
+  require 'sidekiq/testing'
+  Sidekiq::Testing.inline!
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: "unix://#{Rails.root}/tmp/sockets/redis.sock", :namespace => 'headbanger' }
 end
