a7726540a8c9afcfa2ec79f38d5ecd65ab1fd169
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-05-26 21:01:29 +0200

    Page router

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,6 +16,27 @@ headbanger.config(function($routeProvider) {
       templateUrl: template('signin.html'),
       controller: 'SigninController'
     })
+    .when('/signup', {
+      templateUrl: template('signup.html')
+    })
+    .when('/whats-new', {
+      templateUrl: template('whats-new.html')
+    })
+    .when('/by-genre', {
+      templateUrl: template('by-genre.html')
+    })
+    .when('/by-country', {
+      templateUrl: template('by-country.html')
+    })
+    .when('/by-label', {
+      templateUrl: template('by-label.html')
+    })
+    .when('/my-account', {
+      templateUrl: template('my-account.html')
+    })
+    .when('/preferences', {
+      templateUrl: template('preferences.html')
+    })
     .otherwise({
       redirectTo: '/app'
     });
---
+++ app/assets/templates/my-account.html
@@ -0,0 +1,1 @@
+<h1>My account</h1>
