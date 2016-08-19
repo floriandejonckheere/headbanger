034c1f54ddb7f528dc01011e68a5fa8915e11345
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-08-19 15:31:07 +0200

    API version

--- config/initializers/musicbrainz.rb
+++ config/initializers/musicbrainz.rb
@@ -1,8 +1,8 @@
 MusicBrainz.configure do |c|
   # Application identity (required)
-  c.app_name = Rails.application.config.api.name
-  c.app_version = Headbanger::VERSION
-  c.contact = Rails.application.config.api.contact
+  c.app_name = Rails.application.config.api['name']
+  c.app_version = Rails.application.config.api['version']
+  c.contact = Rails.application.config.api['contact']
 
   # Cache config (optional)
   c.cache_path = '/tmp/musicbrainz-cache'
