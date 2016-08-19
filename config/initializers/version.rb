990f8c6d98fec09d1bf77a69cfb0a0e6bc653f4e
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-08-19 14:30:07 +0200

    Update version to include dev env

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,8 +3,13 @@ module Headbanger
   VERSION_MAJOR = 0
   VERSION_MINOR = 1
   VERSION_PATCH = 0
-  VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}"
 
-  # Node format version
-  NODE_VERSION = 1
+  if Rails.env.production?
+    VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}"
+  else
+    VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}-dev"
+  end
+
+  # Graph format version
+  GRAPH_VERSION = 1
 end
