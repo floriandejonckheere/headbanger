de33527e6690c8a322a692c3d84505ece17f871c
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-08-21 13:31:00 +0200

    Remove admin privilege from database

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20160530093451) do
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
     t.string   "name"
-    t.boolean  "admin",                  default: false,   null: false
     t.string   "provider",               default: "email", null: false
     t.string   "tokens"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
