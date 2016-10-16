196b7941c7fdf534454ab5c49c3c65ce76486c26
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-10-16 11:18:57 +0200

    Inline Devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
           :registerable,
-          :recoverable,
-          :confirmable,
+          #~ :recoverable,
+          #~ :confirmable,
           :trackable,
           :timeoutable,
           :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@
 Rails.application.routes.draw do
   root 'app#index'
 
-  devise_for :users
+  # Custom Devise routes
+  scope :users do
+    # Sessions
+    post '/sign_in', :to => 'devise/sessions#create', :as => 'user_session'
+    delete '/sign_out', :to => 'devise/sessions#destroy', :as => 'destroy_user_session'
+
+    # Registrations
+    post '/', :to => 'devise/registrations#create', :as => 'user_registration'
+    delete '/', :to => 'devise/registrations#destroy'
+  end
 
   # mount Sidekiq::Web => '/sidekiq' if Rails.env == 'development'
 end
