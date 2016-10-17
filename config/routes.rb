30f59970df4c280bac4f5affd1b618245557d6a7
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-10-17 09:18:45 +0200

    Revert custom Devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,7 @@
 Rails.application.routes.draw do
   root 'app#index'
 
-  # Custom Devise routes
-  scope :users do
-    # Sessions
-    post '/sign_in', :to => 'devise/sessions#create', :as => 'user_session'
-    delete '/sign_out', :to => 'devise/sessions#destroy', :as => 'destroy_user_session'
-
-    # Registrations
-    post '/', :to => 'devise/registrations#create', :as => 'user_registration'
-    delete '/', :to => 'devise/registrations#destroy'
-  end
+  devise_for :users
 
   # mount Sidekiq::Web => '/sidekiq' if Rails.env == 'development'
 end
