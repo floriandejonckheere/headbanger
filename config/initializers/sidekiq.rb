1167debd6283285403bc34eed2599e50e839605b
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-01-25 14:47:00 +0100

    Don't specify Redis URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,3 @@ if Rails.env.development?
   require 'sidekiq/testing'
   Sidekiq::Testing.inline!
 end
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: "unix://#{Rails.root}/tmp/sockets/redis.sock", :namespace => 'headbanger' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "unix://#{Rails.root}/tmp/sockets/redis.sock", :namespace => 'headbanger' }
-end
