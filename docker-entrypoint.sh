a8c331a89c4f5efa5ec08748759f6dee912ea376
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-01-28 14:47:50 +0100

    Allow using Neo4j's Bolt protocol

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@
 N=5
 
 while true; do
-    curl -s -I -u "#{ENV['NEO4J_USER']}:#{ENV['NEO4J_PASS']}" "http://${NEO4J_HOST}:${NEO4J_PORT}" | grep -q "200 OK"
+    curl -s -I -u "#{ENV['NEO4J_USER']}:#{ENV['NEO4J_PASS']}" "http://${NEO4J_HOST}:7474" | grep -q "200 OK"
 
     if [[ $? -eq 0 ]]; then
         echo "Neo4j is up, starting app"
