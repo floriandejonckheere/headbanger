4c57463c0c6ae66c1aff76786a2a6c59bdeef168
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-02-16 17:06:53 +0100

    Don't complain about bad connection in development

--- config/initializers/active_musicbrainz.rb
+++ config/initializers/active_musicbrainz.rb
@@ -1,2 +1,11 @@
-ActiveMusicbrainz::Model::Base.establish_connection :musicbrainz
-ActiveMusicbrainz.init
+begin
+  ActiveMusicbrainz::Model::Base.establish_connection :musicbrainz
+  ActiveMusicbrainz.init
+rescue => e
+  # Reraise exception if not in development
+  if Rails.env.development?
+    Rails.logger.warn 'Connection to MusicBrainz database NOT established'
+  else
+    raise e
+  end
+end
