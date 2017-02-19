e10bcc093df0b181b9723c74451687a7436b080c
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-02-19 19:06:49 +0100

    Show external providers

--- db/migrate/20170219122350_create_authentication_providers.rb
+++ db/migrate/20170219122350_create_authentication_providers.rb
@@ -2,13 +2,14 @@ class CreateAuthenticationProviders < ActiveRecord::Migration
   def change
     create_table "authentication_providers", :force => true do |t|
       t.string   "name"
+      t.string   "display_name"
       t.datetime "created_at",                 :null => false
       t.datetime "updated_at",                 :null => false
     end
     add_index "authentication_providers", ["name"], :name => "index_name_on_authentication_providers"
-    AuthenticationProvider.create(name: 'facebook')
-    AuthenticationProvider.create(name: 'google_oauth2')
-    AuthenticationProvider.create(name: 'twitter')
+    AuthenticationProvider.create(name: 'facebook', display_name: 'Facebook')
+    AuthenticationProvider.create(name: 'google_oauth2', display_name: 'Google')
+    AuthenticationProvider.create(name: 'twitter', display_name: 'Twitter')
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@ ActiveRecord::Schema.define(version: 20170219122351) do
 
   create_table "authentication_providers", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "display_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.index ["name"], name: "index_name_on_authentication_providers"
   end
 
@@ -46,8 +47,6 @@ ActiveRecord::Schema.define(version: 20170219122351) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "provider"
-    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
