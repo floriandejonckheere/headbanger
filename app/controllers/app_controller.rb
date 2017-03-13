7888277bf030022b4f591f72ee1817f69e18e483
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-03-13 13:46:47 +0100

    Sample cover art URL

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -3,7 +3,7 @@ class AppController < ApplicationController
 
   # Routes
   def discover
-    @a_flame_to_the_ground_beneath = Graph::Release.find_by! :musicbrainz_key => '4c448bb7-d79f-4cc9-b25a-7e9752eaa562'
+    @releases = Graph::Release.all
   end
 
   def terms_of_service; end
