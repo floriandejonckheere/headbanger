9edccadf0e0a13186765c53038b6ac086283117d
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-04-27 23:17:47 +0200

    Refactor Graph::ArtistService

--- app/services/graph/artist_service.rb
+++ app/services/graph/artist_service.rb
@@ -1,92 +1,108 @@
 module Graph
-  class ArtistService < GraphService
-    include Sidekiq::Worker
-
-    ##
-    # Finds or create an instance, and enqueues an update if necessary
-    #
-    def self.find(musicbrainz_key)
-      instance = Graph::Artist.find_or_initialize_by :musicbrainz_key => musicbrainz_key
-      ArtistService.perform_async(musicbrainz_key) if instance.stale?
-
-      return instance
+  class ArtistService < AbstractService
+    def model
+      Graph::Artist
     end
 
-    def perform(musicbrainz_key)
-      instance = Graph::Artist.find_or_initialize_by :musicbrainz_key => musicbrainz_key
-      return if instance.fresh?
+    def enumerate
+      MetalArchives::Artist.all
+    end
 
-      Neo4j::ActiveBase.run_transaction do |tx|
-        begin
-          ##
-          # Update data
-          #
-          @musicbrainz = Graph::Data::MusicbrainzService.get_artist instance.musicbrainz_key
+    def update_instance
+      source_instance
+      update_attributes
+      update_associations
 
-          # No :metal_archives_key, try to find one
-          unless instance.metal_archives_key
-            @musicbrainz.l_artist_urls.each do |link|
-              if link.url.url =~ /(http:\/\/)?(www.)?metal-archives.com/
-                @instance.metal_archives_key = link.url.url.split('/').last
-                break
-              end
-            end
+      @instance.save
+    end
+
+    protected
+
+    def source_instance
+      # Find MA instance
+      @metal_archives = MetalArchives::Artist.find! @instance.metal_archives_key
+
+      # Try to find musicbrainz instance
+      find_musicbrainz_instance
+    end
+
+    def update_attributes
+      # Gender
+      @instance.gender = @metal_archives.gender&.to_s&.capitalize
+
+      # Date of birth
+      @instance.date_of_birth = Date.new @musicbrainz.begin_date_year.to_i,
+                                         @musicbrainz.begin_date_month.to_i,
+                                         @musicbrainz.begin_date_day.to_i
+
+      # Date of death
+      @instance.date_of_death = Date.new @musicbrainz.end_date_year.to_i,
+                                         @musicbrainz.end_date_month.to_i,
+                                         @musicbrainz.end_date_day.to_i
+
+      @instance.biography = sanitize @metal_archives.biography
+    end
+
+    def update_associations
+      # Country
+      @instance.country = @metal_archives.country&.alpha3
+
+      # Names
+      @instance.names.destroy_all
+
+      primary_name = @musicbrainz.name
+      @instance.names << Graph::Name.new(:name => primary_name, :primary_name => true)
+
+      names = []
+      (@musicbrainz.credit_names + @musicbrainz.aliases + @metal_archives.aliases).each do |acn|
+        names << acn.name unless acn.name == primary_name
+      end
+
+      names.uniq.each { |name| @instance.names << Graph::Name.new(:name => name) }
+
+      # TODO: Releases
+    end
+
+    def find_musicbrainz_instance
+      query = ActiveMusicbrainz::Model::Artist.joins(:area)
+                  .joins(:type)
+                  .where 'artist.name ILIKE :name', :name => @metal_archives.name
+
+      if query.one?
+        @musicbrainz = query.first
+        raise Headbanger::IncorrectTypeError unless @musicbrainz.type.name === 'Person'
+
+        return
+      end
+
+      query = query.where 'area.name ILIKE :name', :name => @metal_archives.country.name
+
+      if query.one?
+        @musicbrainz = query.first
+        raise Headbanger::IncorrectTypeError unless @musicbrainz.type.name === 'Person'
+
+        return
+      end
+
+      query.each do |band|
+        next if band.type.name == 'Person'
+
+        band.urls.each do |url|
+          if url.url =~ /(http:\/\/)?(www.)?metal-archives.com/
+            metal_archives_key = url.url.split('/').last
+            break
           end
-
-          @metal_archives = Graph::Data::MetalArchivesService.get_artist instance.metal_archives_key
-
-          ##
-          # Update attributes
-          #
-
-          # Gender
-          instance.gender = @musicbrainz.gender.name
-
-          # Date of birth
-          instance.date_of_birth = Date.new @musicbrainz.begin_date_year.to_i,
-                                            @musicbrainz.begin_date_month.to_i,
-                                            @musicbrainz.begin_date_day.to_i
-
-          # Date of death
-          instance.date_of_death = Date.new @musicbrainz.end_date_year.to_i,
-                                            @musicbrainz.end_date_month.to_i,
-                                            @musicbrainz.end_date_day.to_i
-
-          # Biography
-          instance.biography = ActionView::Base.full_sanitizer.sanitize @metal_archives.biography
-
-          ##
-          # Update associations
-          #
-
-          # Country
-          instance.country = @metal_archives.country
-
-          # Names
-          instance.names.destroy_all
-
-          primary_name = @musicbrainz.name
-          instance.names << Graph::Name.new(:name => primary_name, :primary => true)
-
-          names = []
-          @musicbrainz.credit_names.each { |acn| names << acn.name unless acn.name == primary_name }
-          @musicbrainz.aliases.each { |a| names << a.name unless acn.name == primary_name }
-
-          names.uniq.each { |name| @instance.names << Graph::Name.new(:name => name) }
-
-          # TODO: Groups
-          # TODO: Releases
-          # @musicbrainz.release_groups.each do |release_group|
-          #   next unless release_group.type == 'Album'
-          #   instance.releases << ReleaseService.find_or_create(release_group.gid)
-          # end
-        rescue => e
-          logger.error e
-          tx.mark_failed
         end
 
-        instance.save
+        if metal_archives_key === @instance.metal_archives_key
+          @instance.musicbrainz_key = @musicbrainz.gid
+
+          @musicbrainz = band
+          break
+        end
       end
+
+      raise Headbanger::NoKeyError unless @musicbrainz
     end
   end
 end
