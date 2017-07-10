a05d6ce5b40534f8ddbca31e17d77a05dcbc723a
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-10 08:46:28 +0200

    Implement views for (un-)rating

--- app/controllers/concerns/rateable_controller.rb
+++ app/controllers/concerns/rateable_controller.rb
@@ -7,19 +7,28 @@ module RateableController
     def rate
       current_user.rated << resource
 
-      redirect :back
+      redirect_to :back
     end
 
     def unrate
       current_user.rated.delete resource
 
-      redirect :back
+      redirect_to :back
     end
 
     private
 
     def resource
       @resource ||= controller_name.classify.constantize.find params[:id]
+    end
+
+    def back_url
+      if params[:redirect]
+        @url = params[:redirect]
+      else
+        @url = request.env["HTTP_REFERER"]
+        @url ||= url_for :controller => 'app', :action => 'home'
+      end
     end
   end
 end
