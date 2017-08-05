b33569cac02f7a409d9cb29efc3a23295a10edf2
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-05 11:00:47 +0200

    Add JR data resources

---
+++ app/resources/artist_resource.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class ArtistResource < ApplicationResource
+  immutable
+
+  ##
+  # Attributes
+  #
+  attributes :name,
+             :aliases,
+             :country,
+             :date_of_birth,
+             :date_of_death,
+             :biography
+
+  ##
+  # Relationships
+  #
+  has_many :groups
+  has_many :releases
+
+  ##
+  # Filters
+  #
+  ##
+  # Methods
+  #
+  def name
+    @model.names.find_by(:primary => true).name
+  end
+
+  def aliases
+    @model.names.where(:primary => false).pluck :name
+  end
+
+  def country
+    @model.country&.country
+  end
+end
---
+++ app/resources/group_resource.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class GroupResource < ApplicationResource
+  immutable
+
+  ##
+  # Attributes
+  #
+  attributes :name,
+             :aliases,
+             :country,
+             :year_formed,
+             :description,
+             :status,
+             :genres
+
+  ##
+  # Relationships
+  #
+  has_many :releases
+  has_many :artists
+
+  ##
+  # Filters
+  #
+  ##
+  # Methods
+  #
+  def name
+    @model.names.find_by(:primary => true).name
+  end
+
+  def aliases
+    @model.names.where(:primary => false).pluck :name
+  end
+
+  def country
+    @model.country&.country
+  end
+
+  def genres
+    @model.genres.pluck(:name)
+  end
+end
