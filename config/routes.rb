92be666f68048a6eb33ad5de87e0b90bf370c7b3
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-13 08:24:47 +0200

    Namespace API

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,20 @@
 Rails.application.routes.draw do
   root 'application#index'
 
-  ##
-  # User resources
-  #
-  jsonapi_resources :users, :only => %i[show]
-  jsonapi_resources :lists
-  # jsonapi_resource :queue
-  jsonapi_resources :search, :only => %i[create]
+  scope :api do
+    ##
+    # User resources
+    #
+    jsonapi_resources :users, :only => %i[show]
+    jsonapi_resources :lists
+    # jsonapi_resource :queue
+    jsonapi_resources :search, :only => %i[create]
 
-  ##
-  # Data pages
-  #
-  jsonapi_resources :groups
-  jsonapi_resources :artists
-  jsonapi_resources :releases
+    ##
+    # Data pages
+    #
+    jsonapi_resources :groups
+    jsonapi_resources :artists
+    jsonapi_resources :releases
+  end
 end
