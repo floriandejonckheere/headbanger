b27c516d4984235f5ff92312d7fd1c816e04a748
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-10-28 22:21:03 +0200

    Refactor MainContainer

---
+++ web/src/components/main/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './Footer.css'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="hb-footer uk-text-center uk-padding uk-padding-remove-horizontal uk-text-small">
+        <a href="https://florian.dejonckhee.re/" className="uk-link-reset">&copy; 2017 Florian Dejonckheere</a>
+      </footer>
+    )
+  }
+}
--- web/src/components/main/Main.js
+++ web/src/components/main/Main.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react'
 
 import './Main.css'
 
-class Main extends Component {
+import Footer from 'components/main/Footer'
+
+export default class Main extends Component {
   render() {
     return (
       <div className="hb-main uk-padding uk-height-1-1" data-uk-height-viewport="expand: true">
-        <strong>Title</strong>
+        { this.props.children }
 
-        <footer className="hb-footer uk-text-center uk-padding uk-padding-remove-horizontal uk-text-small">
-          <a href="https://florian.dejonckhee.re/" className="uk-link-reset">&copy; 2017 Florian Dejonckheere</a>
-        </footer>
+        <Footer />
       </div>
     )
   }
 }
-
-export default Main
