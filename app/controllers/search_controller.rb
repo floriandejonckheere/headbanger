78d0fdecf056c54251ac6cf64e412c8991c24a20
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-10-28 13:54:50 +0200

    Infer resource class from record

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
+require 'extensions/jsonapi/utils/response/formatters'
+
 class SearchController < ApplicationController
   # GET /search/:id
   def show
-    results = Name.search(params[:id]).map(&:performer) +
-              Genre.search(params[:id]) +
-              Release.search(params[:id])
-
-    @resources = SearchResource.resources_for results, context
+    @resources = Name.search(params[:id]).map(&:performer) +
+                 Genre.search(params[:id]) +
+                 Release.search(params[:id])
 
     jsonapi_render :json => @resources
   end
--- app/resources/search_resource.rb
+++ app/resources/search_resource.rb
@@ -6,30 +6,5 @@ class SearchResource < ApplicationResource
   ##
   # Attributes
   #
-  attributes :query, :display_name
-
   key_type :string
-
-  ##
-  # Relationships
-  #
-  ##
-  # Filters
-  #
-  ##
-  # Methods
-  #
-  def fetchable_fields
-    super - %i[query]
-  end
-
-  def display_name
-    @model._model.display_name
-  end
-
-  class << self
-    def resource_type_for(model)
-      model.class.name.downcase
-    end
-  end
 end
