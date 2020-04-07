9945a534b93b0658da79cd7b62acdaef4f2a233c
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-04-07 21:57:49 +0200

    Add Buefy

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+
+import 'buefy/dist/buefy.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
