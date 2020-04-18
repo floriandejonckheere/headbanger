2ac379c65a43a606129051818df997b24af2857f
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-04-18 11:51:59 +0200

    Run tests in Neo4j transaction

--- spec/support/neo4j.rb
+++ spec/support/neo4j.rb
@@ -3,9 +3,16 @@
 RSpec.configure do |config|
   config.include Neo4j::RSpec::Matchers
 
-  config.before do
+  config.before :all do
     Neo4j::ActiveBase
       .current_session
       .query("MATCH (n) WHERE NOT n:`Neo4j::Migrations::SchemaMigration` AND NOT n:`Genre` DETACH DELETE n")
+  end
+
+  config.around do |example|
+    Neo4j::ActiveBase.run_transaction do |tx|
+      example.run
+      tx.mark_failed
+    end
   end
 end
