35c5b3b2719b533e7761f9e4291d2c82f8711ea5
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-04-26 17:28:18 +0200

    Seed dislikes

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -6,6 +6,7 @@ neo4j_transaction do
   rand(100..200).times do
     user = FactoryBot.create(:user)
 
+    # Likes
     genres = Graph::Genre.all.to_a.sample(rand(1..3))
     themes = Graph::Theme.all.to_a.sample(rand(5..10))
 
@@ -20,6 +21,22 @@ neo4j_transaction do
                     else
                       group.artists.to_a.sample
                     end
+    end
+
+    # Dislikes
+    genres = Graph::Genre.all.to_a.sample(rand(1..3))
+
+    groups = genres.map(&:groups).flatten.uniq
+
+    groups.each do |group|
+      n = rand(3)
+      user.dislikes << if n.zero?
+                         group
+                       elsif n == 1
+                         group.releases.to_a.sample
+                       else
+                         group.artists.to_a.sample
+                       end
     end
 
     user.save!
