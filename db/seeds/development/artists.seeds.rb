4b39ce8df47d1eb981acdcc39e399b8c45394488
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-01 18:15:02 +0200

    Adjust RuboCop rules

--- db/seeds/development/artists.seeds.rb
+++ db/seeds/development/artists.seeds.rb
@@ -1,21 +1,7 @@
 # frozen_string_literal: true
 
-neo4j_transaction do
-  Rails.logger.info "-- Creating artists"
+ActiveRecord::Base.transaction do
+  puts "-- Creating artists"
 
-  rand(25..50).times do
-    artist = FactoryBot.create(:artist)
-
-    groups = [Graph::Group.all.to_a.sample(rand(2..4)), FactoryBot.build_list(:group, rand(0..5))].flatten
-    artist.groups = groups
-
-    releases = [
-      Graph::Release.all.to_a.sample(rand(2..4)),
-      FactoryBot.build_list(:release, rand(5..10), group: groups.sample),
-    ].flatten
-
-    artist.releases = releases
-
-    artist.save!
-  end
+  rand(25..50).times { FactoryBot.create(:artist) }
 end
--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :artist, class: "Artist" do
     name { FFaker::Name.name }
     alt_names { [] }
-    description { "A #{DateTime.current.year - born_at.year} year old #{gender} artist from #{country.name}." }
+    description { "A #{DateTime.current.year - born_at.year} year old #{gender} artist from #{ISO3166::Country[country].name}." }
     born_at { FFaker::Time.between(20.years.ago, 50.years.ago) }
     died_at { [FFaker::Time.between(born_at, DateTime.current), nil, nil].sample }
     gender { %w(male female).sample }
