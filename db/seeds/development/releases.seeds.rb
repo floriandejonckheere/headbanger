6db933b0cf616db0a1e08e8618c4be96f8c9dfaf
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-01 18:15:25 +0200

    Refactor releases seeds

--- db/seeds/development/releases.seeds.rb
+++ db/seeds/development/releases.seeds.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-neo4j_transaction do
-  Rails.logger.info "-- Creating releases"
+ActiveRecord::Base.transaction do
+  puts "-- Creating releases"
 
   rand(75..100).times do
     release = FactoryBot.create(:release)
 
-    artists = [Graph::Artist.all.to_a.sample(rand(1..3)), FactoryBot.build_list(:artist, rand(1..3))].flatten
+    artists = [Artist.order("RANDOM()").limit(rand(1..3)), FactoryBot.build_list(:artist, rand(1..3))].flatten
     release.artists = artists
+
+    release.groups = Group.order("RANDOM()").limit(rand(0..1))
 
     release.save!
   end
