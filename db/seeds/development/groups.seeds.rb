f08e03c5553f608ca95c804e8ab71b52a08fdddd
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-01 18:15:13 +0200

    Refactor group seeds

--- db/seeds/development/groups.seeds.rb
+++ db/seeds/development/groups.seeds.rb
@@ -1,23 +1,16 @@
 # frozen_string_literal: true
 
-neo4j_transaction do
-  Rails.logger.info "-- Creating groups"
+ActiveRecord::Base.transaction do
+  puts "-- Creating groups"
 
   rand(25..50).times do
     group = FactoryBot.create(:group)
 
-    artists = [Graph::Artist.all.to_a.sample(rand(2..4)), FactoryBot.build_list(:artist, rand(1..5))].flatten
+    artists = [Artist.order("RANDOM()").limit(rand(2..4)), FactoryBot.build_list(:artist, rand(1..5))].flatten
     group.artists = artists
 
-    releases = [
-      Graph::Release.all.to_a.sample(rand(1..5)),
-      FactoryBot.build_list(:release, rand(0..5), group: group),
-    ].sample
-
-    group.releases = releases
-
-    group.themes = FactoryBot.create_list(:theme, rand(0..5))
-    group.genres = FactoryBot.create_list(:genre, rand(1..2))
+    group.themes = FactoryBot.create_list(:theme, rand(0..5)).uniq
+    group.genres = FactoryBot.create_list(:genre, rand(1..2)).uniq
 
     group.save!
   end
--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :group, class: "Graph::Group" do
+  factory :group, class: "Group" do
     name { FFaker::Metal.group }
     alt_names { [] }
-    description { "A band from #{country.name}." }
+    description { "A band from #{ISO3166::Country[country].name}." }
     formed_at { FFaker::Time.between(50.years.ago, DateTime.current) }
-    state { Graph::Group::STATES.sample }
+    status { Group::STATUSES.sample }
     country { ISO3166::Country.codes.sample }
   end
 end
