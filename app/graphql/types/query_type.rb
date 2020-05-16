c1bb1c30d8a976530cd312e85262a2a390c60619
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-16 13:48:37 +0200

    Add user query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,14 +2,17 @@
 
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :user,
+          UserType,
+          null: true do
+      description "Find a user by ID"
+      argument :id,
+               ID,
+               required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def user(id:)
+      User.find(id)
     end
   end
 end
