4a136d70f06b23afccd57cf525f62eb57b4f8d9b
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-16 12:06:13 +0200

    Configure GraphQL API

---
+++ app/controllers/graphql_controller.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class GraphqlController < ApplicationController
+  # If accessing from outside this domain, nullify the session
+  # This allows for outside API access while preventing CSRF attacks,
+  # but you'll have to authenticate your user separately
+  # protect_from_forgery with: :null_session
+
+  def execute
+    variables = ensure_hash(params[:variables])
+    query = params[:query]
+    operation_name = params[:operationName]
+    context = {
+      current_user: current_user,
+    }
+    result = HeadbangerSchema.execute query,
+                                      variables: variables,
+                                      context: context,
+                                      operation_name: operation_name
+
+    render json: result
+  rescue StandardError => e
+    raise e unless Rails.env.development?
+
+    handle_error_in_development e
+  end
+
+  private
+
+  def ensure_hash(ambiguous_param)
+    case ambiguous_param
+    when String
+      if ambiguous_param.present?
+        ensure_hash(JSON.parse(ambiguous_param))
+      else
+        {}
+      end
+    when Hash, ActionController::Parameters
+      ambiguous_param
+    when nil
+      {}
+    else
+      raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+    end
+  end
+
+  def handle_error_in_development(error)
+    logger.error error.message
+    logger.error error.backtrace.join("\n")
+
+    render json: { errors: [{ message: error.message, backtrace: error.backtrace }], data: {} }, status: 500
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     get "/", to: "application#index"
   end
+
+  # GraphQL API
+  post "/graphql", to: "graphql#execute"
 end
