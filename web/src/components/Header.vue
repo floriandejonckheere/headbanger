f5c52dc2e35d590083c3943f6b840d9a2b17315a
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-22 10:35:10 +0200

    Rearrange header

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav class="uk-navbar-container">
+    <nav class="uk-navbar-container uk-navbar-transparent uk-background-default uk-box-shadow-small">
       <div class="uk-container">
         <div class="uk-navbar">
           <div class="uk-navbar-left">
@@ -9,22 +9,20 @@
             </router-link>
           </div>
           <div class="uk-navbar-center">
+            <div class="uk-inline">
+              <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: search"></span>
+              <input
+                class="uk-input uk-form-blank hb-header-search hb-header-search"
+                type="text"
+                placeholder="Search music...">
+            </div>
+          </div>
+          <div class="uk-navbar-right">
             <ul class="uk-navbar-nav">
               <li class="uk-active"><a href="#">Home</a></li>
               <li><a href="#">Discover</a></li>
               <li><a href="#">Explore</a></li>
             </ul>
-          </div>
-          <div class="uk-navbar-right">
-            <div>
-              <a class="uk-navbar-toggle" uk-search-icon href="#"></a>
-              <div class="uk-drop" uk-drop="mode: click; pos: left-center; offset: 0">
-                <form class="uk-search uk-search-navbar uk-width-1-1">
-                  <input class="uk-search-input" type="search" placeholder="Search for images"
-                         autofocus>
-                </form>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -33,6 +31,8 @@
 </template>
 
 <script>
+import './Header.scss';
+
 export default {
   name: 'Header',
 };
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 module.exports = {
   pluginOptions: {
     apollo: {
       lintGQL: true,
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        styles: path.resolve(__dirname, './src/assets/styles/'),
+      },
     },
   },
 };
