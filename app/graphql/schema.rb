e025bad6e19f7b8c035d85f51c283b1f7aea9fae
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-22 17:54:35 +0200

    Implement missing Schema methods

--- app/graphql/schema.rb
+++ app/graphql/schema.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Schema < GraphQL::Schema
-  mutation Types::MutationType
   query Types::QueryType
+  mutation Types::MutationType
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
@@ -10,4 +10,24 @@ class Schema < GraphQL::Schema
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
+
+  def self.resolve_type(_abstract_type, object, _context)
+    type_class = "::Types::#{object.class}Type".safe_constantize
+
+    raise ArgumentError, "Cannot resolve type for class #{object.class.name}" if type_class.blank?
+
+    type_class
+  end
+
+  def self.object_from_id(id, _context)
+    return if id.blank?
+
+    record_class_name, record_id = GraphQL::Schema::UniqueWithinType.decode(id)
+    record_class = record_class_name.safe_constantize
+    record_class&.find_by id: record_id
+  end
+
+  def self.id_from_object(object, _type, _context)
+    GraphQL::Schema::UniqueWithinType.encode(object.class.name, object.id)
+  end
 end
