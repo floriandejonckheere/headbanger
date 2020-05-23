1135f2271a185ee85a2fbe4f4833d46ce28d2bbb
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-23 09:53:02 +0200

    Add genres requests spec

---
+++ spec/requests/genres/get_genres_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+RSpec.describe "Get genres" do
+  it "returns genres alphabetically" do
+    query_string = <<-GRAPHQL
+    query {
+      genres {
+        id
+        name
+        description
+        groups { id name }
+      }
+    }
+    GRAPHQL
+
+    group = create(:group, name: "my_group")
+    # Use 0_ as a prefix, because the database already contains seeded genres
+    genre_b = create(:genre, name: "0_my_genre_b", groups: [group])
+    genre_a = create(:genre, name: "0_my_genre_a", groups: [group])
+    genre_c = create(:genre, name: "0_my_genre_c", groups: [group])
+
+    post graphql_path, params: { query: query_string }
+
+    expect(response_body.dig("data", "genres", 0, "id")).to eq genre_a.id
+    expect(response_body.dig("data", "genres", 0, "name")).to eq "0_my_genre_a"
+    expect(response_body.dig("data", "genres", 0, "description")).to eq genre_a.description
+    expect(response_body.dig("data", "genres", 0, "groups", 0, "name")).to eq "my_group"
+
+    expect(response_body.dig("data", "genres", 1, "id")).to eq genre_b.id
+    expect(response_body.dig("data", "genres", 2, "id")).to eq genre_c.id
+  end
+end
