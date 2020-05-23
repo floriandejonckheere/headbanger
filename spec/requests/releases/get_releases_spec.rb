39be85584cf3ccc3ca63ffb6c21e33f9b8448a08
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-23 09:17:57 +0200

    Check if releases returns alphabetically

--- spec/requests/releases/get_releases_spec.rb
+++ spec/requests/releases/get_releases_spec.rb
@@ -16,13 +16,18 @@ RSpec.describe "Get releases" do
 
     group = create(:group, name: "my_group")
     artist = create(:artist, name: "my_artist")
-    release = create(:release, name: "my_release", groups: [group], artists: [artist])
+    release_b = create(:release, name: "my_release_b", groups: [group], artists: [artist])
+    release_a = create(:release, name: "my_release_a", groups: [group], artists: [artist])
+    release_c = create(:release, name: "my_release_c", groups: [group], artists: [artist])
 
     post graphql_path, params: { query: query_string }
 
-    expect(response_body.dig("data", "releases", 0, "id")).to eq release.id
-    expect(response_body.dig("data", "releases", 0, "name")).to eq "my_release"
+    expect(response_body.dig("data", "releases", 0, "id")).to eq release_a.id
+    expect(response_body.dig("data", "releases", 0, "name")).to eq "my_release_a"
     expect(response_body.dig("data", "releases", 0, "groups", 0, "name")).to eq "my_group"
     expect(response_body.dig("data", "releases", 0, "artists", 0, "name")).to eq "my_artist"
+
+    expect(response_body.dig("data", "releases", 1, "id")).to eq release_b.id
+    expect(response_body.dig("data", "releases", 2, "id")).to eq release_c.id
   end
 end
