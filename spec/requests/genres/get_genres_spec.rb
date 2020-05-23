b3f3b7619fb01b78f2779b5268f0f09499816214
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-23 09:55:45 +0200

    Extract query string in specs

--- spec/requests/genres/get_genres_spec.rb
+++ spec/requests/genres/get_genres_spec.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe "Get genres" do
-  it "returns genres alphabetically" do
-    query_string = <<-GRAPHQL
-    query {
-      genres {
-        id
-        name
-        description
-        groups { id name }
+  let(:query) do
+    <<-GRAPHQL
+      query {
+        genres {
+          id
+          name
+          description
+          groups { id name }
+        }
       }
-    }
     GRAPHQL
+  end
 
+  it "returns genres alphabetically" do
     group = create(:group, name: "my_group")
     # Use 0_ as a prefix, because the database already contains seeded genres
     genre_b = create(:genre, name: "0_my_genre_b", groups: [group])
     genre_a = create(:genre, name: "0_my_genre_a", groups: [group])
     genre_c = create(:genre, name: "0_my_genre_c", groups: [group])
 
-    post graphql_path, params: { query: query_string }
+    post graphql_path, params: { query: query }
 
     expect(response_body.dig("data", "genres", 0, "id")).to eq genre_a.id
     expect(response_body.dig("data", "genres", 0, "name")).to eq "0_my_genre_a"
--- spec/requests/releases/get_releases_spec.rb
+++ spec/requests/releases/get_releases_spec.rb
@@ -1,26 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe "Get releases" do
-  it "returns releases alphabetically" do
-    query_string = <<-GRAPHQL
-    query {
-      releases {
-        id
-        name
-        releasedAt
-        groups { id name }
-        artists { id name }
+  let(:query) do
+    <<-GRAPHQL
+      query {
+        releases {
+          id
+          name
+          releasedAt
+          groups { id name }
+          artists { id name }
+        }
       }
-    }
     GRAPHQL
+  end
 
+  it "returns releases alphabetically" do
     group = create(:group, name: "my_group")
     artist = create(:artist, name: "my_artist")
     release_b = create(:release, name: "my_release_b", groups: [group], artists: [artist])
     release_a = create(:release, name: "my_release_a", groups: [group], artists: [artist])
     release_c = create(:release, name: "my_release_c", groups: [group], artists: [artist])
 
-    post graphql_path, params: { query: query_string }
+    post graphql_path, params: { query: query }
 
     expect(response_body.dig("data", "releases", 0, "id")).to eq release_a.id
     expect(response_body.dig("data", "releases", 0, "name")).to eq "my_release_a"
