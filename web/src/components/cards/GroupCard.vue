c42cc6cd8bfbd6f895daf40d86bedd6989f065b1
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-23 14:12:28 +0200

    Use Card in GroupCard

--- web/src/components/cards/GroupCard.vue
+++ web/src/components/cards/GroupCard.vue
@@ -1,24 +1,12 @@
 <template>
-  <div class="uk-card uk-card-default uk-box-shadow-small uk-background-default">
-    <div class="uk-card-media-top">
-      <img src="https://ia601209.us.archive.org/25/items/mbid-a86017c0-09d9-4edd-81d3-767ac56ed3ff/mbid-a86017c0-09d9-4edd-81d3-767ac56ed3ff-14459102581_thumb500.jpg" alt="">
-    </div>
-    <div class="uk-card-body uk-text-left uk-padding-small">
-      <div class="uk-text-bold uk-text-break">
-        <router-link to="#" class="uk-link-text" v-text="group.name"></router-link>
-      </div>
-      <div class="uk-text-small">
-        By <router-link to="#" v-text="group.name"></router-link>
-      </div>
-      <div class="uk-text-right uk-margin-small-top">
-        <Rating v-bind:rating="null" />
-      </div>
-    </div>
-  </div>
+  <Card
+    :title="group.name"
+    :description="group.name"
+  />
 </template>
 
 <script>
-import Rating from '@/components/Rating.vue';
+import Card from './Card.vue';
 
 export default {
   name: 'GroupCard',
@@ -26,7 +14,7 @@ export default {
     group: Object,
   },
   components: {
-    Rating,
+    Card,
   },
 };
 </script>
