f5eeda26320b72fac527ed7494ab0c19cd8c5e32
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-27 09:01:55 +0200

    Enable Postgres unaccent extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_085432) do
+ActiveRecord::Schema.define(version: 2020_05_27_085433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "artists", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
