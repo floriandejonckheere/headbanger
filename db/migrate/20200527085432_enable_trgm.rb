90be6c38404de34fad6e48b7fcf21116ea4d7214
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-05-27 09:00:58 +0200

    Enable Postgres trigram extension

---
+++ db/migrate/20200527085432_enable_trgm.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class EnableTrgm < ActiveRecord::Migration[6.0]
+  def change
+    enable_extension "pg_trgm"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_063855) do
+ActiveRecord::Schema.define(version: 2020_05_27_085432) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
