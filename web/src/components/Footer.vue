f1bc20a0a90cad087a5e968af980c402a46851af
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-06-25 22:29:53 +0200

    Add footer link target

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="uk-section uk-section-default uk-section-small">
     <div class="uk-container">
       <p class="uk-text-small uk-text-center uk-link-muted">
-        <a href="https://florian.dejonckhee.re">
+        <a href="https://florian.dejonckhee.re" target="_blank">
           Made with <ion-icon name="heart" size="small" class="uk-inline uk-text-danger" /> in Belgium
         </a>
       </p>
