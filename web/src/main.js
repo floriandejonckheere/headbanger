c2afe31853b9a6d4dca23a3183b57b62f4aea33c
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-09-06 17:39:07 +0200

    Enable Vue Sentry integration

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,7 @@ import UIkit from 'uikit';
 
 import * as Sentry from '@sentry/browser';
 import { Vue as VueIntegration } from '@sentry/integrations';
+import { Integrations } from '@sentry/tracing';
 
 import InfiniteLoading from 'vue-infinite-loading';
 
@@ -27,7 +28,15 @@ Vue.config.ignoredElements = [/^ion-/];
 Sentry.init({
   dsn: process.env.VUE_APP_SENTRY_DSN,
   release: version,
-  integrations: [new VueIntegration({ Vue, attachProps: true })],
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true,
+      logErrors: true,
+    }),
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1,
 });
 
 Sentry.configureScope((scope) => {
