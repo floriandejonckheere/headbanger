f428902c735d8cd1abff0bee2ac25416ec795e05
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-09-11 21:02:34 +0200

    Let NGINX substitute Sentry environment variables on runtime

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
+  config.dsn = ENV["RAILS_APP_SENTRY_DSN"]
   config.async = ->(event) { SentryJob.perform_later(event) }
   config.release = Headbanger::Version::STRING
   config.tags = { instance: ENV.fetch("INSTANCE", "headbanger") }
