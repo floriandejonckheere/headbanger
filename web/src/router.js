890c25f3fb7609d2fb2aecd72d81b962e5182e9e
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-09-13 10:10:46 +0200

    Rename router

--- web/src/router.js
+++ web/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -8,34 +7,34 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: import(/* webpackChunkName: "home" */ './views/Home.vue'),
   },
   {
     path: '/discover',
     name: 'discover',
-    component: () => import(/* webpackChunkName: "discover" */ '../views/Discover.vue'),
+    component: () => import(/* webpackChunkName: "discover" */ './views/Discover.vue'),
   },
   {
     path: '/browse',
     name: 'browse',
-    component: () => import(/* webpackChunkName: "browse" */ '../views/Browse.vue'),
+    component: () => import(/* webpackChunkName: "browse" */ './views/Browse.vue'),
   },
   {
     path: '/search',
     name: 'search',
-    component: () => import(/* webpackChunkName: "search" */ '../views/Search.vue'),
+    component: () => import(/* webpackChunkName: "search" */ './views/Search.vue'),
   },
 
   // Genres
   {
     path: '/browse/genres',
     name: 'genres',
-    component: () => import(/* webpackChunkName: "genres" */ '../views/browse/Genres.vue'),
+    component: () => import(/* webpackChunkName: "genres" */ './views/browse/Genres.vue'),
   },
   {
     path: '/browse/genres/:id',
     name: 'genre',
-    component: () => import(/* webpackChunkName: "genre" */ '../views/browse/Genre.vue'),
+    component: () => import(/* webpackChunkName: "genre" */ './views/browse/Genre.vue'),
     props: true,
   },
 
@@ -43,7 +42,7 @@ const routes = [
   {
     path: '/browse/artists/:id',
     name: 'artist',
-    component: () => import(/* webpackChunkName: "artist" */ '../views/browse/Artist.vue'),
+    component: () => import(/* webpackChunkName: "artist" */ './views/browse/Artist.vue'),
     props: true,
   },
 
@@ -51,19 +50,19 @@ const routes = [
   {
     path: '/auth/signin',
     name: 'signin',
-    component: () => import(/* webpackChunkName: "signin" */ '../views/auth/Signin.vue'),
+    component: () => import(/* webpackChunkName: "signin" */ './views/auth/Signin.vue'),
   },
   {
     path: '/auth/signup',
     name: 'signup',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/auth/Signup.vue'),
+    component: () => import(/* webpackChunkName: "signup" */ './views/auth/Signup.vue'),
   },
 
   // Catchall
   {
     path: '*',
     name: 'not_found',
-    component: () => import(/* webpackChunkName: "not_found" */ '../views/errors/NotFound.vue'),
+    component: () => import(/* webpackChunkName: "not_found" */ './views/errors/NotFound.vue'),
   },
 ];
 
