508129f9b2c4303ebb3a45e3f2c3702306dc3d08
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-10-01 10:48:48 +0200

    Set default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "headbanger_production"
 
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST") }
+
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
