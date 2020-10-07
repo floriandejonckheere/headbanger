20dcd2ff67483c7302fa76572d70fd2fb5fc6e7c
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-10-07 20:08:46 +0200

    Add update user mutation

---
+++ app/graphql/mutations/users/update.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Users
+    class Update < BaseMutation
+      argument :id,
+               ID,
+               required: true
+
+      argument :name,
+               String,
+               required: false
+
+      argument :email,
+               String,
+               required: false
+
+      argument :password,
+               String,
+               required: false
+
+      argument :country,
+               String,
+               required: false
+
+      field :user,
+            Types::UserType,
+            null: true
+
+      field :errors,
+            [Types::ErrorType],
+            null: false
+
+      def resolve(id:, **attributes)
+        user = User.find(id)
+
+        user.update(attributes)
+
+        { user: user, errors: user.graphql_errors }
+      end
+    end
+  end
+end
---
+++ spec/requests/users/update_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe "Sign up" do
+  let(:query) do
+    <<-GRAPHQL
+      mutation updateUser ($id: ID!, $name: String, $email: String, $password: String, $country: String) {
+        updateUser (input: { id: $id, name: $name, email: $email, password: $password, country: $country }) {
+          errors { message path }
+
+          user {
+              id
+              name
+              email
+              country
+          }
+        }
+      }
+    GRAPHQL
+  end
+
+  let!(:user) { create(:user) }
+
+  it "returns an error when object is not found" do
+    update_user(id: "notfound", name: "John")
+
+    expect(response).to have_error_codes "USER_ERROR"
+    expect(response_body.dig(:data, :updateUser, :user)).to be_nil
+  end
+
+  it "updates a user" do
+    update_user(id: user.id, name: "John")
+
+    expect(response).not_to have_errors
+    expect(response_body.dig(:data, :updateUser, :user, :name)).to eq "John"
+  end
+
+  def update_user(variables)
+    post graphql_path,
+         params: {
+           query: query,
+           variables: variables,
+         },
+         headers: user.create_new_auth_token
+  end
+end
