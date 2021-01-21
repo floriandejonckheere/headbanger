d28a91a1e6f9688c1ce43ce78d0d0522f8867117
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-01-21 19:33:14 +0100

    Update CD workflow to use Docker Swarm

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -4,5 +4,5 @@ Raven.configure do |config|
   config.dsn = ENV["RAILS_APP_SENTRY_DSN"]
   config.async = ->(event) { SentryJob.perform_later(event) }
   config.release = Headbanger::Version::STRING
-  config.tags = { instance: ENV.fetch("INSTANCE", "headbanger") }
+  config.tags = { instance: ENV.fetch("VUE_APP_INSTANCE", "development") }
 end
