58addfc375b9f0232e7ba6f4785b6b83d207924e
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-01-22 17:09:39 +0100

    Generate keys in /app/

--- web/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-openssl req -nodes -new -x509 \
-  -keyout /etc/nginx/conf.d/client.key \
-  -out /etc/nginx/conf.d/client.pem \
-  -subj "/C=BE/ST=/L=/O=CN=headbang.re"
-
-nginx -g "daemon off;"
