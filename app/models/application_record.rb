4c8bd1019c6bfcd839faa33f08e4af8aa53cffd1
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-01-27 21:33:56 +0100

    GraphQL error: show full message

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,10 +4,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def graphql_errors
-    errors.map do |attribute, message|
+    errors.map do |error|
       {
-        message: message,
-        path: ["attributes", attribute.to_s.camelize(:lower)],
+        message: error.full_message,
+        path: ["attributes", error.attribute.to_s.camelize(:lower)],
         extensions: { code: Errors::BaseError::ERROR_CODES.fetch(:user_error) },
       }
     end
