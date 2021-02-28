09895e7cd9d10f80c9467af3525bd2c85807d083
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-02-28 19:32:57 +0100

    Add dinja gem

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie" if Rails.env.development? # Required for GraphiQL
 # require "rails/test_unit/railtie"
+require "dinja/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
