265faf5adeea8680ecb323864f384358a7de33ef
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-03-14 22:06:21 +0100

    Drop ETL specs

--- spec/etl/steps/type_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Steps::Type do
-  subject(:step) { described_class.new("foo_type") }
-
-  it "enhances the data with a type" do
-    expect(step.run({ id: "foo" })).to eq id: "foo", type: "foo_type"
-  end
-end
