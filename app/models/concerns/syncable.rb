a2bd73cdbefc6f953dfa8f997b3e01deefbba3da
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-03-20 18:08:59 +0100

    Add synced/expired methods to syncable concern

--- app/models/concerns/syncable.rb
+++ app/models/concerns/syncable.rb
@@ -3,6 +3,17 @@
 module Syncable
   extend ActiveSupport::Concern
 
+  class_methods do
+    def synced
+      where(synced_at: Headbanger.config.data_expires_in.ago..)
+    end
+
+    def expired
+      where(synced_at: nil)
+        .or(where(synced_at: ..Headbanger.config.data_expires_in.ago))
+    end
+  end
+
   def synced?
     synced_at&.> Headbanger.config.data_expires_in.ago
   end
--- spec/support/shared_examples/syncable.rb
+++ spec/support/shared_examples/syncable.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples "it is syncable" do |_model|
+  describe ".synced/.expired" do
+    it "returns synced and expired records" do
+      synced = create(described_class.name.underscore.to_sym, synced_at: 10.seconds.ago)
+      unsynced = create(described_class.name.underscore.to_sym, synced_at: nil)
+      expired = create(described_class.name.underscore.to_sym, synced_at: 1.year.ago)
+
+      expect(described_class.synced).to include synced
+      expect(described_class.synced).not_to include unsynced, expired
+
+      expect(described_class.expired).to include unsynced, expired
+      expect(described_class.expired).not_to include synced
+    end
+  end
+
   describe "#synced?/#expired?" do
     context "when it has not been synced" do
       before { subject.synced_at = nil }
