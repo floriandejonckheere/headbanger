416b5494b6fd174fa2e98dba4ebc47af2154f7e1
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-03-29 23:18:01 +0200

    Drop builder classes

--- app/builders/theme_builder.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ThemeBuilder < ApplicationBuilder
-  def initialize(description:)
-    super(name: Theme.name_for(description))
-  end
-end
--- spec/builders/theme_builder_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.xdescribe ThemeBuilder, type: :builder do
-  subject(:theme) { described_class.new(description: "Epic").call }
-
-  it { is_expected.to have_attributes name: "epic", description: "Epic" }
-end
