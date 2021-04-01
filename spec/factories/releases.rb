53b7fcb95f278d93b0ab182adf5eb008c52b0e09
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-01 17:34:43 +0200

    Update releases factory

--- spec/factories/releases.rb
+++ spec/factories/releases.rb
@@ -6,5 +6,12 @@ FactoryBot.define do
     description { "A release from #{released_at.year}." }
     released_at { FFaker::Time.between(50.years.ago, DateTime.current) }
     synced_at { FFaker::Time.datetime }
+
+    metal_archives_key { FFaker::Guid.guid }
+    musicbrainz_key { FFaker::Guid.guid }
+
+    trait :expired do
+      synced_at { (Headbanger.config.data_expires_in + 1.day).ago }
+    end
   end
 end
