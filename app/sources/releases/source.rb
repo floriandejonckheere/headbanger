e3e54751082499bd472b0fe46933a087af6630f9
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-03 17:09:54 +0200

    Don't implement release find_musicbrainz_key

--- app/sources/releases/source.rb
+++ app/sources/releases/source.rb
@@ -43,34 +43,6 @@ module Releases
       @musicbrainz ||= Headbanger.container.resolve("releases.musicbrainz", musicbrainz_key)
     end
 
-    def find_musicbrainz_key
-      # groups = ActiveBrainz::Artist
-      #   .includes(:artist_type, :artist_aliases, artist_area: [:area_type, :area_iso_3166_1, :area_iso_3166_2])
-      #   .where(artist_type: { name: "Group" })
-      #
-      # # Filter by name
-      # groups = groups
-      #   .where(name: metal_archives.name)
-      #
-      # return groups.first.gid if groups.one?
-      #
-      # # Filter by country
-      # groups = groups
-      #   .where(artist_area: { area_type: { name: "Country" }, iso_3166_1: { code: metal_archives.country } })
-      #
-      # # Or subdivision
-      # subdivisions = ISO3166::Country[metal_archives.country].subdivisions.keys.map { |s| "#{metal_archives.country}-#{s}" }
-      #
-      # groups = groups
-      #   .or(groups.where(artist_area: { area_type: { name: "Subdivision" }, iso_3166_2: { code: subdivisions } }))
-      #
-      # return groups.first.gid if groups.one?
-      #
-      # # Filter by aliases
-      # groups = groups
-      #   .where(artist_aliases: { name: metal_archives.alt_names }) if metal_archives.alt_names.any?
-      #
-      # groups.first.gid if groups.one?
-    end
+    def find_musicbrainz_key; end
   end
 end
